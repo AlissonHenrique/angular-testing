6c32c13f70a2b784b51f3721800b0273b4a6b780
Alisson Henrique <alisson-henrique@live.com>
2025-12-04 16:32:40 -0300

    first commit

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -1,10 +1,10 @@
-import {CoursesService} from './courses.service';
-import {TestBed} from '@angular/core/testing';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import {COURSES, findLessonsForCourse} from '../../../../server/db-data';
-import {Course} from '../model/course';
-import { HttpErrorResponse, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { TestBed } from "@angular/core/testing";
+import { CoursesService } from "./courses.service";
 
+import {  HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { HttpErrorResponse, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { COURSES, findLessonsForCourse } from "../../../../server/db-data";
+import { Course } from "../model/course";
 
 describe('CoursesService', () => {
 
@@ -21,106 +21,78 @@ describe('CoursesService', () => {
         provideHttpClientTesting()
     ]
 });
-
         coursesService = TestBed.inject(CoursesService),
         httpTestingController = TestBed.inject(HttpTestingController);
+})
+it('should retrieve all courses', () => {
+  coursesService.findAllCourses().subscribe(courses => {
+     expect(courses).toBeTruthy('No courses returned');
+     expect(courses.length).toBe(12, "incorrect number of courses");
 
-    });
+    const course = courses.find(course => course.id == 12);
+    expect(course.titles.description).toBe("Angular Testing Course");
 
-    it('should retrieve all courses', () => {
+  });
 
-        coursesService.findAllCourses()
-            .subscribe(courses => {
+   const req = httpTestingController.expectOne('/api/courses');
+   expect(req.request.method).toEqual("GET");
+   req.flush({payload: Object.values(COURSES)});
+});
 
-                expect(courses).toBeTruthy('No courses returned');
 
-                expect(courses.length).toBe(12,
-                    "incorrect number of courses");
+it('should find a course by id', () => {
+  coursesService.findCourseById(12).subscribe(course => {
+    expect(course).toBeTruthy();
+    expect(course.id).toBe(12);
+  })
+  const req = httpTestingController.expectOne('/api/courses/12');
+  expect(req.request.method).toEqual("GET");
+  req.flush(COURSES[12]);
+})
 
-                const course = courses.find(course => course.id == 12);
 
-                expect(course.titles.description).toBe(
-                    "Angular Testing Course");
+it('should save the course data', () => {
+    const changes: Partial<Course> = {titles: {description: 'Testing Course'}};
+    coursesService.saveCourse(12,changes).subscribe(course => {
+        expect(course.id).toBe(12);
+  })
 
-            });
 
-        const req = httpTestingController.expectOne('/api/courses');
-
-        expect(req.request.method).toEqual("GET");
-
-        req.flush({payload: Object.values(COURSES)});
-
-    });
-
-    it('should find a course by id', () => {
-
-        coursesService.findCourseById(12)
-            .subscribe(course => {
-
-                expect(course).toBeTruthy();
-                expect(course.id).toBe(12);
-
-            });
-
-        const req = httpTestingController.expectOne('/api/courses/12');
-
-        expect(req.request.method).toEqual("GET");
-
-        req.flush(COURSES[12]);
-
-    });
-
-    it('should save the course data', () => {
-
-        const changes :Partial<Course> =
-            {titles:{description: 'Testing Course'}};
-
-        coursesService.saveCourse(12, changes)
-            .subscribe(course => {
-
-                expect(course.id).toBe(12);
-
-            });
-
-        const req = httpTestingController.expectOne('/api/courses/12');
-
+   const req = httpTestingController.expectOne('/api/courses/12');
         expect(req.request.method).toEqual("PUT");
-
-        expect(req.request.body.titles.description)
+         expect(req.request.body.titles.description)
             .toEqual(changes.titles.description);
 
-        req.flush({
+    req.flush({
             ...COURSES[12],
             ...changes
-        })
-
     });
 
-    it('should give an error if save course fails', () => {
 
-        const changes :Partial<Course> =
-            {titles:{description: 'Testing Course'}};
+})
 
-        coursesService.saveCourse(12, changes)
-            .subscribe(
-                () => fail("the save course operation should have failed"),
 
-                (error: HttpErrorResponse) => {
-                    expect(error.status).toBe(500);
-                }
-            );
-
-        const req = httpTestingController.expectOne('/api/courses/12');
-
+it('should give an error if save course fails', () => {
+  const changes: Partial<Course> = {titles: {description: 'Testing Course'}};
+      coursesService.saveCourse(12,changes).subscribe(() => {
+        fail('the save course operation should have failed.');
+      }, (error:HttpErrorResponse) => {
+        expect(error.status).toBe(500);
+      }
+  )
+       const req = httpTestingController.expectOne('/api/courses/12');
         expect(req.request.method).toEqual("PUT");
 
-        req.flush('Save course failed', {status:500,
-            statusText:'Internal Server Error'});
-    });
+        req.flush('Save course failed', {
+            status: 500,
+            statusText: 'Internal Server Error'
+        });
 
-    it('should find a list of lessons', () => {
+})
 
-        coursesService.findLessons(12)
+it('should retrieve all courses', () => {
+
+   coursesService.findLessons(12)
             .subscribe(lessons => {
 
                 expect(lessons).toBeTruthy();
@@ -143,27 +115,14 @@ describe('CoursesService', () => {
             payload: findLessonsForCourse(12).slice(0,3)
         });
 
-
-    });
-
-    afterEach(() => {
-
-        httpTestingController.verify();
-    });
-
 });
 
 
 
 
+   afterEach(() => {
+        httpTestingController.verify();
+    });
 
 
-
-
-
-
-
-
-
-
-
+})
